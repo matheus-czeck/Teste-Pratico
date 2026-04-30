d4e27cf071618e1d7967fca4f5654a1e477aa0a8
unknown <matheusczeckcz@gmail.com>
2026-04-30 12:27:11 -0300

    iciciando logica controllers

--- Back-end/src/controllers/PedidosController.ts
+++ Back-end/src/controllers/PedidosController.ts
@@ -3,12 +3,62 @@ import prisma from "../config/db";
 
 class PedidosController {
   static async listarPedidos(req: Request, res: Response) {
-    const listaPedidos = await prisma.pedido.findMany()
-    res.status(200).send(listaPedidos)
+    const listaPedidos = await prisma.pedido.findMany();
+
+    if (listaPedidos !== null) {
+      res.status(200).send(listaPedidos);
+    } else {
+      res.send({
+        message: "Nenhum pedido foi encontrado",
+        status: 404,
+      });
+    }
   }
-  static criarPedido(req: Request, res: Response) {}
-  static alterarPedido(req: Request, res: Response) {}
-  static deletarPedido(req: Request, res: Response) {}
+  static async criarPedido(req: Request, res: Response) {
+    const pedido = req.params;
+
+    const criarPedido = await prisma.pedido.create();
+
+    if (criarPedido) {
+      res.send({
+        message: "Pedido criado com sucesso!",
+        status: 201,
+      });
+    } else {
+      res.send({
+        message: "Algo deu errado ao criar o pedido!",
+        status: 400,
+      });
+    }
+  }
+  static async alterarPedido(req: Request, res: Response) {
+    const alterarPedidoId = req.params;
+
+    const pedidoId = await prisma.pedido.findById(alterarPedidoId.id);
+
+    const pedidoAlterado = await pedidoId.replace(alterarPedidoId);
+
+    if (pedidoAlterado) {
+      res.send({
+        message: "Pedido alterado com sucesso!",
+        status: 200,
+      });
+    } else {
+      res.send({
+        message: "Algo deu errado ao criar o pedido!",
+        status: 400,
+      });
+    }
+  }
+  static async deletarPedido(req: Request, res: Response) {
+    const deletarPedido = req.params
+
+    const pedidoDeletado =  await prisma.pedido.deleteById(deletarPedido.id);
+  
+    if(pedidoDeletado){
+      res.send({message: })
+    }
+  }
 }
 
 export default PedidosController;
--- Back-end/src/controllers/ProdutosController.ts
+++ Back-end/src/controllers/ProdutosController.ts
@@ -2,16 +2,23 @@ import prisma from "../config/db";
 import { RequestHandler, Request, Response } from "express";
 
 export class ProdutosController {
-
-  static async listarProdutos(req: Request, res: Response){
+  static async listarProdutos(req: Request, res: Response) {
     try {
       const todosProdutos = await prisma.produto.findMany();
-      res.send(todosProdutos);
 
+      if (todosProdutos !== null) {
+        res.send({
+          json: todosProdutos,
+          status: 200,
+        });
+      } else {
+        res.send({
+          message: "Não há produtos cadastrados!",
+          status: 400,
+        });
+      }
     } catch (err) {
-      res.send(err)
+      res.send(`Aconteceu um erro interno: ${err}`);
     }
   }
-
-  
 }
